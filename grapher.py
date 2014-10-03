4b86ec1b1beb73d868a0663f9aeff5dda7c0a3bf
whyoh <whyoh@entrophilia.com>
2014-10-03 22:25:53 +0100

    full pycharm de-orange

--- grapher.py
+++ grapher.py
@@ -5,11 +5,11 @@ import networkx
 from numpy import array as vector
 from numpy.linalg import norm
 from random import uniform
-from gi.repository import Gtk, GooCanvas, GObject, Gio
+from gi.repository import Gtk, GooCanvas, GObject
 from datetime import datetime
 
 
-class graph():
+class Graph():
     def __init__(self):
         # UI settings
         self.blobsize = 8.
@@ -23,6 +23,9 @@ class graph():
         self.scale = 1.
         self.minscale = 0.75
         self.maxscale = 2.
+        self.last = datetime.now()
+        self.frame = 0
+        self.unpinnedframe = 0
 
         # physics
         self.mindist = 10.
@@ -42,7 +45,6 @@ class graph():
         self.canvas = GooCanvas.Canvas(has_tooltip=True)
         self.canvas.set_property('background-color-rgb', 0xe8e8e8)
         self.stage = self.canvas.get_root_item()
-        settings = Gio.Settings('org.gnome.desktop.interface')
         self.window = Gtk.Window()
         self.window.set_resizable(False)
         self.window.add(self.canvas)
@@ -70,8 +72,10 @@ class graph():
             self.scale = 1. / xscale
         else:
             self.scale = 1. / yscale
-        if self.scale < self.minscale: self.scale = self.minscale
-        if self.scale > self.maxscale: self.scale = self.maxscale
+        if self.scale < self.minscale:
+            self.scale = self.minscale
+        if self.scale > self.maxscale:
+            self.scale = self.maxscale
         self.top += ((self.scale * (maxy + miny) - self.height) / 2. - self.top) / 5.
         self.left += ((self.scale * (maxx + minx) - self.width) / 2. - self.left) / 5.
         for n in self.graph.node:
@@ -86,7 +90,8 @@ class graph():
         self.centre()
         for n in self.graph.node:
             for m in self.graph[n]:
-                if m > n: continue
+                if m > n:
+                    continue
                 self.graph[n][m]['strength'] = self.default_strength
             # set item physical attributes
             self.graph.node[n]['mass'] = self.default_mass
@@ -95,7 +100,8 @@ class graph():
     def destroy_actors(self):
         for n in self.graph.node:
             for m in self.graph[n]:
-                if m > n: continue
+                if m > n:
+                    continue
                 if 'actor' in self.graph[n][m]:
                     self.stage.remove_child(self.stage.find_child(self.graph[n][m]['actor']))
                     del self.graph[n][m]['actor']
@@ -106,7 +112,8 @@ class graph():
     def create_actors(self):
         for n in self.graph.node:
             for m in self.graph[n]:
-                if m > n: continue
+                if m > n:
+                    continue
                 # including tooltips causes a memory leak when actors are destroyed (bgo 669688)
                 self.graph[n][m]['actor'] = GooCanvas.CanvasPath(parent=self.stage, stroke_color_rgba=0x11111180,
                                                                  line_width=0.75, title='link')
@@ -120,6 +127,7 @@ class graph():
             self.graph.node[n]['actor'].connect("grab-broken-event", self.unpin)
 
     def button(self, item, target, event):
+        _ = target, event  # shut up pycharm - function prototype defined by Gtk
         self.pin = item
         item.props.fill_color_rgba = 0x3333cc80
         for n in self.graph.node:
@@ -128,7 +136,9 @@ class graph():
                 break
 
     def drag(self, item, target, event):
-        if not self.pin or item != self.pin: return
+        _ = target  # shut up pycharm - function prototype defined by Gtk
+        if not self.pin or item != self.pin:
+            return
         x, item.props.x, item.props.y = event.get_root_coords()
         bs = self.scale * self.blobsize / 2.
         item.props.x -= bs
@@ -137,6 +147,7 @@ class graph():
         self.graph.node[self.pinned]['position'][1] = (item.props.y + self.top + bs) / self.scale
 
     def unpin(self, item, target, event):
+        _ = item, target, event  # shut up pycharm - function prototype defined by Gtk
         self.pin.props.fill_color_rgba = 0x33333380
         self.pin = None
         self.unpinnedframe = min(250, self.unpinnedframe)
@@ -144,21 +155,25 @@ class graph():
     def step(self):
         # apply physics to update velocities
         for n in self.graph.node:
-            if self.graph.node[n]['actor'] is self.pin: continue
+            if self.graph.node[n]['actor'] is self.pin:
+                continue
             force = vector([0., 0.])
             for m in self.graph.node:
-                if m == n: continue
+                if m == n:
+                    continue
 
                 # ## CRITICAL SPEED SECTION ###
                 direction = self.graph.node[n]['position'] - self.graph.node[m]['position']
                 dist = norm(direction)
                 if dist < self.mindist:
-                    if dist == 0.: direction = vector([uniform(-0.5, 0.5), uniform(-0.5, 0.5)])
+                    if dist == 0.:
+                        direction = vector([uniform(-0.5, 0.5), uniform(-0.5, 0.5)])
                     dist = self.mindist
                     newdist = norm(direction)
                     direction *= dist / newdist
                 force += direction * self.graph.node[n]['charge'] * self.graph.node[m]['charge'] / dist ** 3.
-                if m in self.graph[n]: force = force - direction * self.spring * (1. - self.naturallength / dist)
+                if m in self.graph[n]:
+                    force -= direction * self.spring * (1. - self.naturallength / dist)
 
             massy = self.stepsize / self.graph.node[n]['mass']
             dampdv = - self.damping * self.graph.node[n]['velocity'] * norm(self.graph.node[n]['velocity']) * massy
@@ -173,14 +188,16 @@ class graph():
         for n in self.graph.node:
             if self.graph.node[n]['actor'] is not self.pin:
                 self.graph.node[n]['position'] += self.graph.node[n]['velocity'] * self.stepsize
-        if not self.pin: self.align()
+        if not self.pin:
+            self.align()
         bs = self.scale * self.blobsize / 2.
 
         for n in self.graph.node:
             self.graph.node[n]['actor'].props.x = self.scale * self.graph.node[n]['position'][0] - self.left - bs
             self.graph.node[n]['actor'].props.y = self.scale * self.graph.node[n]['position'][1] - self.top - bs
             for m in self.graph[n]:
-                if m > n: continue
+                if m > n:
+                    continue
                 # overwriting the 'data' property directly leaks memory (bgo 669661)
                 self.graph[n][m]['actor'].set_property('data', " ".join([
                     "M", str(self.graph.node[n]['actor'].props.x + bs), str(self.graph.node[n]['actor'].props.y + bs),
@@ -188,7 +205,8 @@ class graph():
 
         # reset every five hundred un-pinned frames
         self.frame += 1
-        if not self.pin: self.unpinnedframe += 1
+        if not self.pin:
+            self.unpinnedframe += 1
         if self.unpinnedframe == 500:
             elapsed = datetime.now() - self.last
             print self.frame / elapsed.total_seconds()
@@ -197,5 +215,5 @@ class graph():
         return True
 
 
-g = graph()
+g = Graph()
 Gtk.main()
