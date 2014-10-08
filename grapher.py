b3920b45fb76455928b52bc6de9003bfe50aec8c
whyoh <whyoh@entrophilia.com>
2014-10-08 11:50:16 +0100

    initial GlooGrapher UI subclass

--- grapher.py
+++ grapher.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-# generate a graph, solve for physics, render with clutter
+# generate a graph, solve for physics, render with sub-class
 
 import networkx
 from numpy import array as vector
 from numpy.linalg import norm
 from random import uniform
-from gi.repository import Gtk, GooCanvas, GObject
 from datetime import datetime
+from vispy import gloo, app
 
 
 class Graph():
@@ -41,19 +41,6 @@ class Graph():
         self.graph = None
         self.create()
 
-        # GooCanvas-based UI
-        self.canvas = GooCanvas.Canvas(has_tooltip=True)
-        self.canvas.set_property('background-color-rgb', 0xe8e8e8)
-        self.stage = self.canvas.get_root_item()
-        self.window = Gtk.Window()
-        self.window.set_resizable(False)
-        self.window.add(self.canvas)
-        self.canvas.set_size_request(self.width, self.height)
-        self.window.connect('destroy', Gtk.main_quit)
-        self.window.show_all()
-        self.create_actors()
-        GObject.idle_add(self.step)
-
     def centre(self):
         for n in self.graph.node:
             self.graph.node[n]['position'] = vector([0., 0.])
@@ -78,15 +65,13 @@ class Graph():
             self.scale = self.maxscale
         self.top += ((self.scale * (maxy + miny) - self.height) / 2. - self.top) / 5.
         self.left += ((self.scale * (maxx + minx) - self.width) / 2. - self.left) / 5.
-        for n in self.graph.node:
-            self.graph.node[n]['actor'].props.height = self.graph.node[n][
-                'actor'].props.width = self.blobsize * self.scale
 
     def create(self):
         self.last = datetime.now()
         self.frame = 0
         self.unpinnedframe = 0
         self.graph = networkx.random_regular_graph(3, 26)  # nice test graph
+#        self.graph = networkx.random_regular_graph(3, 16)  # simple test graph
         self.centre()
         for n in self.graph.node:
             for m in self.graph[n]:
@@ -96,6 +81,59 @@ class Graph():
             # set item physical attributes
             self.graph.node[n]['mass'] = self.default_mass
             self.graph.node[n]['charge'] = self.default_charge
+
+    def step(self):
+        # apply physics to update velocities
+        for n in self.graph.node:
+            force = vector([0., 0.])
+            for m in self.graph.node:
+                if m == n:
+                    continue
+
+                ### CRITICAL SPEED SECTION - expect the code to be a bit convoluted ###
+                direction = self.graph.node[n]['position'] - self.graph.node[m]['position']
+                dist = norm(direction)
+                if dist < self.mindist:
+                    if dist == 0.:
+                        direction = vector([uniform(-0.5, 0.5), uniform(-0.5, 0.5)])
+                    dist = self.mindist
+                    newdist = norm(direction)
+                    direction *= dist / newdist
+                force += direction * self.graph.node[n]['charge'] * self.graph.node[m]['charge'] / dist ** 3.
+                if m in self.graph[n]:
+                    force -= direction * self.spring * (1. - self.naturallength / dist)
+
+            massy = self.stepsize / self.graph.node[n]['mass']
+            dampdv = - self.damping * self.graph.node[n]['velocity'] * norm(self.graph.node[n]['velocity']) * massy
+            if norm(dampdv) > norm(self.graph.node[n]['velocity']):
+                self.graph.node[n]['velocity'] = vector([0., 0.])
+            else:
+                self.graph.node[n]['velocity'] += dampdv
+            self.graph.node[n]['velocity'] += force * massy
+        # update positions
+        for n in self.graph.node:
+            self.graph.node[n]['position'] += self.graph.node[n]['velocity'] * self.stepsize
+        if not self.pin:
+            self.align()
+
+        return True
+
+
+class GooGrapher(Graph):
+    def __init__(self, width, height):
+        from gi.repository import Gtk, GooCanvas, GObject
+
+        Graph.__init__(self)
+        canvas = GooCanvas.Canvas(has_tooltip=True)
+        self.stage = canvas.get_root_item()
+        canvas.set_property('background-color-rgb', 0xe8e8e8)
+        window = Gtk.Window()
+        window.set_resizable(False)
+        window.add(canvas)
+        canvas.set_size_request(width, height)
+        window.connect('destroy', Gtk.main_quit)
+        window.show_all()
+        GObject.idle_add(self.step)
 
     def destroy_actors(self):
         for n in self.graph.node:
@@ -110,6 +148,7 @@ class Graph():
                 del self.graph.node[n]['actor']
 
     def create_actors(self):
+        from gi.repository import GooCanvas
         for n in self.graph.node:
             for m in self.graph[n]:
                 if m > n:
@@ -125,6 +164,11 @@ class Graph():
             self.graph.node[n]['actor'].connect("motion-notify-event", self.drag)
             self.graph.node[n]['actor'].connect("button-release-event", self.unpin)
             self.graph.node[n]['actor'].connect("grab-broken-event", self.unpin)
+
+    def set_actor_size(self, size):
+        for n in self.graph.node:
+            self.graph.node[n]['actor'].props.height =\
+                self.graph.node[n]['actor'].props.width = size
 
     def button(self, item, target, event):
         _ = target, event  # shut up pycharm - function prototype defined by Gtk
@@ -152,57 +196,109 @@ class Graph():
         self.pin = None
         self.unpinnedframe = min(250, self.unpinnedframe)
 
-    def step(self):
-        # apply physics to update velocities
-        for n in self.graph.node:
-            if self.graph.node[n]['actor'] is self.pin:
-                continue
-            force = vector([0., 0.])
-            for m in self.graph.node:
-                if m == n:
-                    continue
+    @staticmethod
+    def run():
+        from gi.repository import Gtk
+        Gtk.main()
 
-                # ## CRITICAL SPEED SECTION ###
-                direction = self.graph.node[n]['position'] - self.graph.node[m]['position']
-                dist = norm(direction)
-                if dist < self.mindist:
-                    if dist == 0.:
-                        direction = vector([uniform(-0.5, 0.5), uniform(-0.5, 0.5)])
-                    dist = self.mindist
-                    newdist = norm(direction)
-                    direction *= dist / newdist
-                force += direction * self.graph.node[n]['charge'] * self.graph.node[m]['charge'] / dist ** 3.
-                if m in self.graph[n]:
-                    force -= direction * self.spring * (1. - self.naturallength / dist)
 
-            massy = self.stepsize / self.graph.node[n]['mass']
-            dampdv = - self.damping * self.graph.node[n]['velocity'] * norm(self.graph.node[n]['velocity']) * massy
-            if norm(dampdv) > norm(self.graph.node[n]['velocity']):
-                self.graph.node[n]['velocity'] = vector([0., 0.])
-                self.graph.node[n]['actor'].props.fill_color_rgba = 0xcc333380
-            else:
-                self.graph.node[n]['velocity'] += dampdv
-                self.graph.node[n]['actor'].props.fill_color_rgba = 0x33333380
-            self.graph.node[n]['velocity'] += force * massy
-        # update positions
-        for n in self.graph.node:
-            if self.graph.node[n]['actor'] is not self.pin:
-                self.graph.node[n]['position'] += self.graph.node[n]['velocity'] * self.stepsize
-        if not self.pin:
-            self.align()
-        bs = self.scale * self.blobsize / 2.
+class GlooGrapher(Graph, app.Canvas):
+    def __init__(self, **kwargs):
+        import numpy
+        Graph.__init__(self)
+        app.Canvas.__init__(self, keys='interactive', **kwargs)
+        self.size = self.width, self.height
+        self.position = 50, 50
+
+        self.data = None
+        self.edges = None
+        self.vbo = gloo.VertexBuffer(self.build_vbo_array_from_graph_nodes())
+        self.index = gloo.IndexBuffer(self.build_ibo_array_from_graph_edges())
+        self.view = numpy.eye(4, dtype=numpy.float32)
+        self.model = numpy.eye(4, dtype=numpy.float32)
+        self.projection = numpy.eye(4, dtype=numpy.float32)
+        self.maxscale *= 4
+
+        self.circle_program = gloo.Program(
+            open("circle_vertex.sl").read(),
+            open("circle_fragment.sl").read()
+        )
+        self.circle_program.bind(self.vbo)
+        self.circle_program['u_size'] = 1
+        self.circle_program['u_antialias'] = 1
+        self.circle_program['u_model'] = self.model
+        self.circle_program['u_view'] = self.view
+        self.circle_program['u_projection'] = self.projection
+
+        self.line_program = gloo.Program(
+            open("line_vertex.sl").read(),
+            open("line_fragment.sl").read()
+        )
+        self.line_program.bind(self.vbo)
+        self.t = app.Timer(connect=self.on_frame, start=True)
+        self.show()
+
+    @staticmethod
+    def on_initialize(event):
+        _ = event  # shut up pycharm - function prototype defined by OpenGL
+        gloo.set_state(
+            clear_color='white', depth_test=False, blend=True, blend_func=('src_alpha', 'one_minus_src_alpha'))
+
+    def on_resize(self, event):
+        self.width, self.height = event.size
+        gloo.set_viewport(0, 0, self.width, self.height)
+
+    def on_draw(self, event):
+        _ = event  # shut up pycharm - function prototype defined by OpenGL
+        gloo.clear(color=True, depth=True)
+        self.line_program.draw('lines', self.index)
+        self.circle_program.draw('points')
+
+    def build_vbo_array_from_graph_nodes(self):
+        import numpy
+        if self.data is None:
+            self.data = numpy.zeros(len(self.graph), dtype=[
+                ('a_position', numpy.float32, 3),
+                ('a_fg_color', numpy.float32, 4),
+                ('a_bg_color', numpy.float32, 4),
+                ('a_size', numpy.float32, 1),
+                ('a_linewidth', numpy.float32, 1),
+            ])
+
+        bs = int(self.blobsize * self.scale)
 
         for n in self.graph.node:
-            self.graph.node[n]['actor'].props.x = self.scale * self.graph.node[n]['position'][0] - self.left - bs
-            self.graph.node[n]['actor'].props.y = self.scale * self.graph.node[n]['position'][1] - self.top - bs
+            node = self.graph.node[n]
+            data = self.data[n]
+            data['a_position'][0] = node['position'][0] / 100.
+            data['a_position'][1] = node['position'][1] / 100.
+            data['a_size'] = bs
+            data['a_bg_color'][0] = 0.75
+            data['a_bg_color'][1] = 0.75 if node['velocity'][0] > 0. or node['velocity'][1] > 0. else 1.
+            data['a_bg_color'][2] = 0.75
+            data['a_bg_color'][3] = 1.
+
+        self.data['a_fg_color'] = 0, 0, 0, 1
+        self.data['a_linewidth'] = 1
+        return self.data
+
+    def build_ibo_array_from_graph_edges(self):
+        import numpy
+        if self.edges is None:
+            self.edges = numpy.random.randint(
+                size=(sum(len(x) for x in self.graph.edge.values()) / 2, 2), low=0, high=len(self.graph)
+            ).astype(numpy.uint32)
+        index = 0
+        for n in self.graph.node:
             for m in self.graph[n]:
                 if m > n:
                     continue
-                # overwriting the 'data' property directly leaks memory (bgo 669661)
-                self.graph[n][m]['actor'].set_property('data', " ".join([
-                    "M", str(self.graph.node[n]['actor'].props.x + bs), str(self.graph.node[n]['actor'].props.y + bs),
-                    "L", str(self.graph.node[m]['actor'].props.x + bs), str(self.graph.node[m]['actor'].props.y + bs)]))
+                self.edges[index][0] = n
+                self.edges[index][1] = m
+                index += 1
+        return self.edges
 
+    def on_frame(self, event):
         # reset every five hundred un-pinned frames
         self.frame += 1
         if not self.pin:
@@ -210,10 +306,19 @@ class Graph():
         if self.unpinnedframe == 500:
             elapsed = datetime.now() - self.last
             print self.frame / elapsed.total_seconds()
-            self.destroy_actors()
             self.create()
-        return True
+            self.edges = None
+            self.data = None
+            self.index = gloo.IndexBuffer(self.build_ibo_array_from_graph_edges())
+
+        self.step()
+        self.vbo.set_data(self.build_vbo_array_from_graph_nodes())
+        self.update(event)
+
+    @staticmethod
+    def run():
+        app.run()
 
 
-g = Graph()
-Gtk.main()
+g = GlooGrapher()
+g.run()
